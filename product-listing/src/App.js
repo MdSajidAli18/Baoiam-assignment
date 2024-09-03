836d000ae0f91fd75df7afeb7bfe421ecd2f6200
Sajid <mdsajidali1803@gmail.com>
2024-09-03 17:35:30 +0530

    Add updated file

--- product-listing/src/App.js
+++ product-listing/src/App.js
@@ -4,7 +4,6 @@ import ProductListing from './components/ProductListing';
 function App() {
   return (
     <div className="App bg-sky-50">
-      {/* <h1 className="text-3xl font-[GreatVibes] italic tracking-tight font-bold text-left my-6 ml-4">Product Listing</h1> */}
       <div className="ml-4">
         <ProductListing />
       </div>
